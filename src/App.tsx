117e7e435cb67b674e2e08f475a49b86ebbc6569
dvcode <daviolivero2103@gmail.com>
2024-04-15 08:18:35 -0400

    Chore: Adding parent containers

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,17 @@ function App() {
 
   return (
     <>
-      <h1 className="text-4xl text-indigo-800">Contador Calorias</h1>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between">
+          <h1 className="text-center font-bold text-white uppercase text-lg">Contador de Calorías</h1>
+        </div>
+      </header>
+
+      <section className="bg-lime-500 p-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <p>Formulario aqui</p>
+        </div>
+      </section>
 
     </>
       
