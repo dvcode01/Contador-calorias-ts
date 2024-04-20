75f68ca75648156f2acbb9b544a24f88c42c6f2f
dvcode <daviolivero2103@gmail.com>
2024-04-19 22:36:24 -0400

    Chore: Iterating and Showing the categories

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-
+import { categories } from "../data/categories"
 
 function Form() {
   return (
@@ -6,12 +6,28 @@ function Form() {
       className="space-y-5 bg-white shadow p-10 rounded-lg"
     >
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="category">Categorías:</label>
+        <label htmlFor="category" className="font-bold">Categorías:</label>
 
         <select name="category" id="category" className="border border-slate-300 p-2 w-full rounded-lg bg-white">
-          
+          {categories.map(categorie => (
+            <option key={categorie.id} value={categorie.id}>{categorie.name}</option>
+          ))}
         </select>
       </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className="font-bold">Actividad:</label>
+
+        <input type="text" id="activity" className="border border-slate-300 rounded-lg p-2" placeholder="Ej: Pasta, Correr, Jugo de Naranja"  />
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">Calorías:</label>
+
+        <input type="number" id="calories" className="border border-slate-300 rounded-lg p-2" placeholder="Calorías Ej: 300 o 500"  />
+      </div>
+
+      <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold text-white uppercase cursor-pointer" value="Guardar comida o ejercicio" />
 
     </form>
   )
