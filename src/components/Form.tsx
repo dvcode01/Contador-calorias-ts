2d69ff09ef98d3c9b89643fd318ca56fad6c179f
dvcode <daviolivero2103@gmail.com>
2024-04-19 23:19:30 -0400

    Chore: Type creation and reset of activity values

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,20 @@
 import { categories } from "../data/categories"
+import { Activity } from "../types";
 import { useState, ChangeEvent } from "react"
 
 function Form() {
-  const [activity, setActivity] = useState({
+  const [activity, setActivity] = useState<Activity>({
     category: 1,
     name: '',
     calories: 0
   });
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+    const isNumberField = ['category', 'calories'].includes(e.target.id);
+
     setActivity({
       ...activity, 
-      [e.target.id]: e.target.value
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value
     });
   }
 
