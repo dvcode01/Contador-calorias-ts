e1bf22e94ba938953638fd92a9f0c8b4871beeb5
dvcode <daviolivero2103@gmail.com>
2024-04-19 22:06:44 -0400

    Style: styling the form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,7 +2,18 @@
 
 function Form() {
   return (
-    <div>Form</div>
+    <form 
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+    >
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category">Categorías:</label>
+
+        <select name="category" id="category" className="border border-slate-300 p-2 w-full rounded-lg bg-white">
+          
+        </select>
+      </div>
+
+    </form>
   )
 }
 
