cefd7a5bdc3e88adf16565a7b855c8f955bcd8fe
dvcode <daviolivero2103@gmail.com>
2024-05-04 21:46:16 -0400

    Chore: show calories burned

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -8,17 +8,24 @@ type CalorieTrackerProps = {
 function CalorieTracker({activities} : CalorieTrackerProps) {
     // Contadores
     const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities]);
-
     
+    
+    const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [activities]);
+
 
     return (
         <>
             <h2 className="text-4xl font-black text-white text-center">Resumen de calorías</h2>
 
-            <div className="flex flex-col items-center md:justify-between gap-5 mt-10">
+            <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
                 <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center text-2xl">
                     <span className="font-black text-6xl text-orange">{caloriesConsumed}</span>
                     Consumidas
+                </p>
+
+                <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center text-2xl">
+                    <span className="font-black text-6xl text-orange">{caloriesBurned}</span>
+                    Quemadas
                 </p>
             </div>
             
